b9227cffdf66e14afcf7538f30909654c43c4455
KonkenBonken <konrad05pettersson@gmail.com>
2022-06-10 01:18:31 +0200

    Added units: dm, cm, mm

--- src/groups.mjs
+++ src/groups.mjs
@@ -4,6 +4,21 @@ const groups = [
 			unit: 'm'
 		},
 		{
+			name: 'decimeter',
+			unit: 'dm',
+			multiply: 10
+		},
+		{
+			name: 'centimeter',
+			unit: 'cm',
+			multiply: 100
+		},
+		{
+			name: 'millimeter',
+			unit: 'mm',
+			multiply: 1000
+		},
+		{
 			name: 'foot',
 			unit: 'ft',
 			multiply: 3.2808399
