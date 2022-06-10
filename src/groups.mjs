09d6450738e2a492ed4ab6b1440015f0b3627611
KonkenBonken <konrad05pettersson@gmail.com>
2022-06-10 02:22:47 +0200

    Shorter formatting

--- src/groups.mjs
+++ src/groups.mjs
@@ -1,53 +1,42 @@
 const groups = [
 	[
 		[{
-				name: 'KiloMeter',
-				multiply: 1e-3
-			},
-			{
-				name: 'Meter',
-				isRoot: true
-			},
-			{
-				name: 'DeciMeter',
-				multiply: 10
-			},
-			{
-				name: 'CentiMeter',
-				multiply: 100
-			},
-			{
-				name: 'MilliMeter',
-				multiply: 1000
-			}
-		],
+			name: 'KiloMeter',
+			multiply: 1e-3
+		}, {
+			name: 'Meter',
+			isRoot: true
+		}, {
+			name: 'DeciMeter',
+			multiply: 10
+		}, {
+			name: 'CentiMeter',
+			multiply: 100
+		}, {
+			name: 'MilliMeter',
+			multiply: 1000
+		}],
 		[{
-				name: 'MIle',
-				multiply: 6.21371192e-4
-			},
-			{
-				name: 'YarD',
-				multiply: 1.0936133
-			},
-			{
-				name: 'FooT',
-				multiply: 3.2808399
-			},
-			{
-				name: 'INch',
-				multiply: 39.3700788,
-			}
-		],
+			name: 'MIle',
+			multiply: 6.21371192e-4
+		}, {
+			name: 'YarD',
+			multiply: 1.0936133
+		}, {
+			name: 'FooT',
+			multiply: 3.2808399
+		}, {
+			name: 'INch',
+			multiply: 39.3700788,
+		}],
 		[{
-				name: 'Light-Year',
-				multiply: 1.05702341e-16
-			},
-			{
-				name: 'astronomical unit',
-				multiply: 6.68458712e-12,
-				unit: 'AU'
-			}
-		]
+			name: 'Light-Year',
+			multiply: 1.05702341e-16
+		}, {
+			name: 'astronomical unit',
+			multiply: 6.68458712e-12,
+			unit: 'AU'
+		}]
 	]
 ];
 
