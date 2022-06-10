7c9995f5a6d87438a6c55d384a2852fb18188750
KonkenBonken <konrad05pettersson@gmail.com>
2022-06-11 00:06:31 +0200

    Fixed bug where value was a string

--- src/Unit.js
+++ src/Unit.js
@@ -7,8 +7,8 @@ class Unit extends Component {
 
   render() {
     return (<input type='number' name={this.props.name} unit={this.props.unit}
-              value={this.props.fromRoot(this.props.rootValue)}
-              onChange={({target})=>this.props.setRoot(this.props.toRoot(target.value))}/>);
+              value={this.props.fromRoot(+this.props.rootValue)}
+              onChange={({target})=>this.props.setRoot(+this.props.toRoot(target.value))}/>);
   }
 }
 
