65776fd5f521138ed533cc2b988bfe1095f55038
KonkenBonken <konrad05pettersson@gmail.com>
2024-08-26 23:15:16 +0200

    add links to convert to or from other units

--- app/units/distance/Distance.tsx
+++ app/units/distance/Distance.tsx
@@ -1,15 +1,16 @@
 "use client";
 import { useState } from "react";
 import units from '@/src/data/distance'
-import LinkSelect from '@/components/LinkSelect';
+import { LinkSelect, LinkList } from '@/components/LinkSelect';
 import { usePathname } from "next/navigation";
 import scss from './distance.module.scss';
+import hasch from "hasch";
 
 function Unit({ unit }: { unit: typeof units[string] }) {
   return <abbr title={unit.name}>{unit.unit}</abbr>
 }
 
-function UnitSelect({ from }: { from?: true }) {
+function UnitList({ from, select }: { from?: true, select?: true }) {
   const current = usePathname().split('/');
 
   const href = (unit: typeof units[string]) => {
@@ -24,12 +25,21 @@ function UnitSelect({ from }: { from?: true }) {
     return current.join('/');
   }
 
-  return <LinkSelect links={
+  return select ? <LinkSelect links={
     Object.values(units).map(unit => ({
       href: href(unit),
       value: [unit.name, unit.unit]
     }))
   } />
+    : <LinkList links={
+      Object.values(units)
+        .sort((a, b) => hasch(a.unit + current, { decimal: true, seed: from }) - hasch(b.unit + current, { decimal: true, seed: from }))
+        .slice(0, 15)
+        .map(unit => ({
+          href: href(unit),
+          value: [unit.name, unit.unit]
+        }))
+    } />
 }
 
 export default function Distance({ from, to }: { from?: string, to?: string }) {
@@ -43,7 +53,7 @@ export default function Distance({ from, to }: { from?: string, to?: string }) {
 
   return (<>
     <h2>Distance unit converter</h2>
-    <div className={scss.subHeader}>Convert distance from <UnitSelect from /> to <UnitSelect /></div>
+    <div className={scss.subHeader}>Convert distance from <UnitList select from /> to <UnitList select /></div>
     {
       !!(!incomplete && fromObj && toObj) &&
       <input type="number" value={input} onInput={e => setInput(e.currentTarget.valueAsNumber || 0)} />
@@ -56,5 +66,13 @@ export default function Distance({ from, to }: { from?: string, to?: string }) {
     }
     <div className={scss.text}>{fromObj.text}</div>
     <div className={scss.text}>{toObj.text}</div>
+    <div>
+      <h4>Convert {fromObj.name} to other units:</h4>
+      <UnitList />
+    </div>
+    <div>
+      <h4>Convert {toObj.name} to other units:</h4>
+      <UnitList from />
+    </div>
   </>);
 }
--- components/LinkSelect.tsx
+++ components/LinkSelect.tsx
@@ -3,21 +3,26 @@
 import Link from 'next/link';
 import scss from './LinkSelect.module.scss';
 import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
-export default function LinkSelect({ links }: { links: { href: string, value: [string, string?] }[] }) {
+const UnitText = (value?: [string, string?]) => value ? value.map(text => (<span key={text}>{text}</span>)) : <span>Click here</span>;
+
+export function LinkList({ links, select }: { links: { href: string, value: [string, string?] }[], select?: true }) {
+  return <ul className={clsx(scss.linkList, !select && scss.list)}>
+    {links.map(({ href, value }) => (
+      <li key={value.join()}>
+        <Link href={href}>{UnitText(value)}</Link>
+      </li>
+    ))}
+  </ul>;
+}
+
+export function LinkSelect({ links }: { links: { href: string, value: [string, string?] }[] }) {
   const path = usePathname();
-
-  const UnitText = (value?: [string, string?]) => value ? value.map(text => (<span key={text}>{text}</span>)) : <span>Click here</span>;
 
   return <div className={scss.linkSelect} tabIndex={0}>
     <span className={scss.current}>{UnitText(links.find(({ href }) => href === path)?.value)}</span>
     <span className={scss.arrow}>▼</span>
-    <ul>
-      {links.map(({ href, value }) => (
-        <li key={value.join()}>
-          <Link href={href}>{UnitText(value)}</Link>
-        </li>
-      ))}
-    </ul>
+    <LinkList links={links} select />
   </div>
 }
