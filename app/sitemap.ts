066982bfa56b6f1c57dc810373cb72041f56c773
KonkenBonken <konrad05pettersson@gmail.com>
2024-09-09 19:06:51 +0200

    add sitemap.xml generator

---
+++ app/sitemap.ts
@@ -0,0 +1,43 @@
+import type { MetadataRoute } from "next";
+import massUnits from "@/src/data/mass";
+import lengthUnits from "@/src/data/length";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const urls = [];
+
+  urls.push(
+    ...Object.keys(lengthUnits).flatMap((a) => [
+      `units/length/${a}/-`,
+      `units/length/-/${a}`,
+      ...Object.keys(lengthUnits).map((b) => `units/length/${a}/${b}`),
+    ])
+  );
+
+  urls.push(
+    ...Object.keys(massUnits).flatMap((a) => [
+      `units/mass/${a}/-`,
+      `units/mass/-/${a}`,
+      ...Object.keys(massUnits).map((b) => `units/mass/${a}/${b}`),
+    ])
+  );
+
+  urls.push("base/-/-", "units/length/-/-", "units/mass/-/-");
+
+  const bases = Array(69)
+    .fill(0)
+    .map((_, i) => (i + 2).toString());
+
+  urls.push(
+    ...bases.flatMap((a) => [
+      `base/${a}/-`,
+      `base/-/${a}`,
+      ...bases.map((b) => `base/${a}/${b}`),
+    ])
+  );
+
+  urls.push("trigonometry/sin", "trigonometry/cos", "trigonometry/tan");
+
+  console.log(urls.length, "urls in sitemap.xml");
+
+  return urls.map((url) => ({ url: `https://utilz.eu/${url}` }));
+}
