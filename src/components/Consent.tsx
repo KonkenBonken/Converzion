816a32f4a4fc25bf7255a0360b87ab4e0112c290
Konrad Pettersson <49250602+KonkenBonken@users.noreply.github.com>
2024-09-27 15:48:24 +0200

    add auto ads

--- src/components/Consent.tsx
+++ src/components/Consent.tsx
@@ -17,5 +17,6 @@ export default function Consent() {
   return <>
     <GoogleAnalytics gaMeasurementId="G-E9SW9RXXSE" />
     <meta name="google-adsense-account" content="ca-pub-2422033382456580" />
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2422033382456580" crossorigin="anonymous" />
   </>;
 }
