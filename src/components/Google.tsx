10f9c1016dcc4a29eec013791c784968800776fe
KonkenBonken <konrad05pettersson@gmail.com>
2024-09-27 19:26:12 +0200

    place Google tags i head

--- src/components/Google.tsx
+++ src/components/Google.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Head from "next/head";
 import { consent, GoogleAnalytics } from "nextjs-google-analytics";
 import { GoogleAdSense } from "nextjs-google-adsense";
 
@@ -15,9 +16,9 @@ export default function Consent() {
   });
 
 
-  return <>
+  return <Head>
     <GoogleAnalytics gaMeasurementId="G-E9SW9RXXSE" />
     <GoogleAdSense publisherId="ca-pub-2422033382456580" />
     <meta name="google-adsense-account" content="ca-pub-2422033382456580" />
-  </>;
+  </Head>;
 }
