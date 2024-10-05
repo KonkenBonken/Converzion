a2fe1af0e95022c817ad6300d23dc484c8d72100
KonkenBonken <konrad05pettersson@gmail.com>
2024-10-05 18:22:24 +0200

    remove Head tag from Google tags

--- src/components/Google.tsx
+++ src/components/Google.tsx
@@ -5,9 +5,9 @@ import { GoogleAnalytics } from "nextjs-google-analytics";
 import { GoogleAdSense } from "nextjs-google-adsense";
 
 export default function Consent() {
-  return <Head>
+  return <>
     <GoogleAnalytics gaMeasurementId="G-E9SW9RXXSE" />
     <GoogleAdSense publisherId="ca-pub-2422033382456580" />
     <meta name="google-adsense-account" content="ca-pub-2422033382456580" />
-  </Head>;
+  </>;
 }
