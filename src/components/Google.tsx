61cd968d8542e56671106083f5dbe1c67384c413
KonkenBonken <konrad05pettersson@gmail.com>
2024-10-14 21:53:28 +0200

    add default consent
    This reverts commit c197d658c0b7df4afc36b8498d64bfff3f3acde3.

--- src/components/Google.tsx
+++ src/components/Google.tsx
@@ -1,10 +1,19 @@
 "use client";
 
-import Head from "next/head";
-import { GoogleAnalytics } from "nextjs-google-analytics";
+import { consent, GoogleAnalytics } from "nextjs-google-analytics";
 import { GoogleAdSense } from "nextjs-google-adsense";
 
 export default function Consent() {
+  if (typeof window !== 'undefined') consent({
+    arg: 'default',
+    params: {
+      ad_storage: 'granted',
+      analytics_storage: 'granted',
+      ad_user_data: 'granted',
+      ad_personalization: 'granted'
+    }
+  });
+
   return <>
     <GoogleAnalytics gaMeasurementId="G-E9SW9RXXSE" />
     <GoogleAdSense publisherId="ca-pub-2422033382456580" />
